0ba4078e28e5595ee19f504624b3665c4aae9044
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-08-25 08:03:56 -0400

    set up firebase, api integrated and works

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { Pinecone } from '@pinecone-database/pinecone';
-import OpenAI from 'openai';
+import { Pinecone } from "@pinecone-database/pinecone";
+import OpenAI from "openai";
 
 const systemPrompt = `
 Description:
@@ -26,66 +26,67 @@ Ensure that the information provided is up-to-date and accurate. If no professor
 `;
 
 export async function POST(req) {
-    const data = await req.json();
-    const pc = new Pinecone({
-        apiKey: process.env.PINECONE_API
-    });
-    const index = pc.index('rag').namespace('ns1');
-    const openai = new OpenAI();
+  const data = await req.json();
+  const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  const index = pc.index("rag").namespace("ns1");
+  const openai = new OpenAI();
 
-    const text = data[data.length - 1].content;
-    const embedding = await openai.embedding.create({
-        model: 'text-embedding-2-small',
-        input: text,
-        encoding_format: 'float',
-    });
+  const text = data[data.length - 1].content;
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text,
+    encoding_format: "float",
+  });
 
-    const results = await index.query({
-        topK: 3,
-        includeMetadata: true,
-        vector: embedding.data[0].embedding,
-    });
+  const results = await index.query({
+    topK: 5,
+    includeMetadata: true,
+    vector: embedding.data[0].embedding,
+  });
 
-    let resultString = '\n\n Returned results from vector:';
-    results.matches.forEach((match) => {
-        resultString += `\n
+  let resultString = "";
+  results.matches.forEach((match) => {
+    resultString += `\n
         Professor: ${match.id}
         Review: ${match.metadata.review}
         Subject: ${match.metadata.subject}
         Stars: ${match.metadata.stars}
         \n\n
         `;
-    });
+  });
 
-    const lastMessage = data[data.length - 1];
-    const lastMessageContent = lastMessage.content + resultString;
-    const completion = await openai.chat.completions.create({
-        messages: [
-            { role: 'system', content: systemPrompt },
-            ...data.slice(0, -1),
-            { role: 'user', content: lastMessageContent }
-        ],
-        model: 'gpt-4-mini',
-        stream: true,
-    });
+  const lastMessage = data[data.length - 1];
+  const lastMessageContent = lastMessage.content + resultString;
+  const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: systemPrompt },
+      ...lastDataWithoutLastMessage,
+      { role: "user", content: lastMessageContent },
+    ],
+    model: "gpt-4o-mini",
+    stream: true,
+  });
 
-    const stream = new ReadableStream({
-        async start(controller) {
-            const encoder = new TextEncoder();
-            try {
-                for await (const chunk of completion) {
-                    const content = chunk.choices[0]?.delta?.content;
-                    if (content) {
-                        const text = encoder.encode(content);
-                        controller.enqueue(text);
-                    }
-                }
-            } catch (err) {
-                controller.error(err);
-            } finally {
-                controller.close();
-            }
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of completion) {
+          const content = chunk.choices[0]?.delta?.content;
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
         }
-    });
-    return NextResponse.json(stream);
+      } catch (err) {
+        controller.error(err);
+      } finally {
+        controller.close();
+      }
+    },
+  });
+  return new NextResponse(stream);
 }
