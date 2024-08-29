c6085b3f6c73cc7a4dc26ad1d219378490984094
Huz_00 <huzya2929@gmail.com>
2024-08-28 20:07:11 -0400

    Clerk auth added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,28 @@
 import Navbar from "./components/Navbar";
 import ChatBox from "./components/ChatBox";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen background flex-col h-screen items-center justify-center'>
-      <Navbar />
-      <ChatBox />
+      <SignedOut>
+        {/* Apply a custom class to the SignInButton */}
+        <SignInButton mode="modal">
+          <button className="custom-signin-button">Sign In</button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex flex-col items-center justify-center w-full">
+          <Navbar />
+          <UserButton />
+          <ChatBox />
+        </div>
+      </SignedIn>
     </main>
   );
 }
