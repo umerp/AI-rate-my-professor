b2b42e0263990e885e1332bbf12ef6dfb7b4e463
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-08-29 10:33:56 -0400

    cleaned navbar

--- app/page.js
+++ app/page.js
@@ -1,17 +1,11 @@
 import Navbar from "./components/Navbar";
 import ChatBox from "./components/ChatBox";
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs';
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen background flex-col h-screen items-center justify-center'>
+    <main className="flex min-h-screen background flex-col h-screen items-center justify-center">
       <SignedOut>
-        {/* Apply a custom class to the SignInButton */}
         <SignInButton mode="modal">
           <button className="custom-signin-button">Sign In</button>
         </SignInButton>
@@ -19,7 +13,6 @@ export default function Home() {
       <SignedIn>
         <div className="flex flex-col items-center justify-center w-full">
           <Navbar />
-          <UserButton />
           <ChatBox />
         </div>
       </SignedIn>
